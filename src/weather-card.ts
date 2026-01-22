b6690024d07036f9f4139b1eef2eba402cd94e08
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-01-22 12:06:53 +0100

    card layout

--- src/weather-card.ts
+++ src/weather-card.ts
@@ -10,6 +10,19 @@ export class WeatherCard extends LitElement {
   render() {
     return html`
       <h1>${this.city}</h1>
+      
+      <div class="mainInfo">
+        <ion-icon name="sunny"></ion-icon>
+        <p class="degrees">15ºC</p>
+        <p class="info1">Soleado</p>
+        <p class="info2">Sensación de 13ºC</p>
+      </div>
+
+      <hr class="bar"/>
+
+      <div class="nextDaysContainer">
+        <p>WIP</p>
+      </div>
     `
   }
 
@@ -17,14 +30,57 @@ export class WeatherCard extends LitElement {
     :host {
       display: flex;
       flex-direction: column;
+      align-items: center;
+      border: 1px solid #f2f2f2;
+      border-radius: 20px;
+      padding: 10px;
+      width: 250px;
+      gap: 10px;
+    }
+
+    h1, p{
+      margin: 0;
+    }
+    p{
+      font-weight: 500;
+    }
+
+    ion-icon{
+      font-size: 5rem;
+    }
+
+    .mainInfo{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 90%;
+    }
+    .degrees{
+     align-self: flex-end;
+     font-size: 1.5rem;
+    }
+    .info1{
+      font-size: 1.2rem;
+    }
+    .info2{
+      margin-top: 10px;
+    }
+
+    .bar{
+      width: 90%;
+      border-radius: 20px;
+      border: 1px solid #f2f2f2;
     }
 
     @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
+      :host{
+        border: 1px solid #242424;
       }
       button {
         background-color: #f9f9f9;
+      }
+      .bar{
+        border: 1px solid #242424;
       }
     }
   `
