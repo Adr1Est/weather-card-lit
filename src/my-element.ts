4581fef835b9f93d7a394f2d602873554b91c761
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-01-22 11:19:03 +0100

    weather-card-lit project WIP

---
+++ src/my-element.ts
@@ -0,0 +1,36 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import litLogo from './assets/lit.svg'
+import viteLogo from '/vite.svg'
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+
+  render() {
+    return html`
+      
+    `
+  }
+
+  static styles = css`
+    :host {
+      display: flex;
+      flex-direction: column;
+    }
+
+    @media (prefers-color-scheme: light) {
+      a:hover {
+        color: #747bff;
+      }
+      button {
+        background-color: #f9f9f9;
+      }
+    }
+  `
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement
+  }
+}
