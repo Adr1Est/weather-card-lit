946062e8f3c0162954af2853c51818ae6acca986
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-01-22 12:55:42 +0100

    fetchWeather.ts

---
+++ src/services/fetchWeather.ts
@@ -0,0 +1,18 @@
+const fetchWeather = async (lat: number, lon: number) => {
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,precipitation,weather_code,cloud_cover&timezone=Europe%2FBerlin&forecast_days=3`);
+    const data = await response.json();
+    return data.current;
+  } catch (error) {
+    console.error(error);
+    return {
+      weather_code: 999,
+      temperature_2m: 999,
+      apparent_temperature: 999,
+    }
+  }
+}
+
+export {
+  fetchWeather,
+}
